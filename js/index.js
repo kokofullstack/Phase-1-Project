99b96860a68edd7e19e6a18f5692777d567143f6
Maung Ko Ko <mwkoko2020@gmail.com>
2023-05-10 17:46:10 -0600

    customize scrollbar in css

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ const dropList = document.querySelectorAll('.drop-list select');
 
 for (let i = 0; i < dropList.length; i++) {
   for (let currency_code in country_code) {
-    //creating currency code as a text and value
+    //selecting USD and MYR by default
     let selected =
       i == 0
         ? currency_code == 'USD'
@@ -11,6 +11,7 @@ for (let i = 0; i < dropList.length; i++) {
         : currency_code == 'MYR'
         ? 'selected'
         : '';
+    //creating currency code as a text and value
     let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
     // inserting option tag
     dropList[i].insertAdjacentHTML('beforeend', optionTag);
