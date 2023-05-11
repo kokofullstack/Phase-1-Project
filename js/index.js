1e1678cc38f6ff6f61626221bf6d528fba9035f9
Maung Ko Ko <mwkoko2020@gmail.com>
2023-05-11 14:42:12 -0600

    change innertext

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 const dropList = document.querySelectorAll('.drop-list select');
-getButton = document.querySelectorAll('form button');
+fromCurrency = document.querySelector('.from select');
+toCurrency = document.querySelector('.to select');
+getButton = document.querySelector('form button');
 
 for (let i = 0; i < dropList.length; i++) {
   for (let currencyCode in countryCode) {
@@ -17,9 +19,9 @@ for (let i = 0; i < dropList.length; i++) {
     // inserting option tag
     dropList[i].insertAdjacentHTML('beforeend', optionTag);
   }
-  dropList[i].addEventListener('change', (e) => {
-    loadFlag(e.target);
-  });
+  // dropList[i].addEventListener('change', (e) => {
+  //   loadFlag(e.target);
+  // });
 }
 
 getButton.addEventListener('click', (e) => {
@@ -31,9 +33,20 @@ getButton.addEventListener('click', (e) => {
 function getExchangeRate() {
   const amount = document.querySelector('.amount input');
   let amountVal = amount.value;
+  //default value is 1 if user don't enter any value or 0
   if (amountVal == '' || amountVal == '0') {
     amount.value = '1';
     amountVal = 1;
   }
-  let url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur.json`;
+
+  let url = `https://open.er-api.com/v6/latest/USD${fromCurrency.value}`;
+  //fetching api response
+  fetch(url)
+    .then((response) => response.json())
+    .then((result) => {
+      let exchangeRate = result.rates[toCurrency.value];
+      let totalExchangeRate = (amountVal * exchangeRate).toFixed(2);
+      const exchangeRateTxt = document.querySelector('.exchange-rate');
+      exchangeRateTxt.innerText = `${amountVal} ${fromCurrency.value} = ${totalExchangeRate} ${toCurrency.value}`;
+    });
 }
